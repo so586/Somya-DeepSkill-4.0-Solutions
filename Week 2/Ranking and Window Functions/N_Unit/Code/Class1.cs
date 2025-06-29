973553f4427f25850c203173588af48f42704ce3
Somya Singh <somya121220@gmail.com>
2025-06-29 18:58:21 +0530

    Add files via upload

---
+++ Week 2/Ranking and Window Functions/N_Unit/Code/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CalcLibrary
+{
+    public class Calculator
+    {
+        public int add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        public double divide(int x, int y)
+        {
+            if (y == 0)
+                throw new DivideByZeroException("Cannot divide by zero");
+            else
+                return (double)x / y;
+        }
+    }
+}
---
+++ Week 2/Ranking and Window Functions/N_Unit/Code/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using CalcLibrary;
+
+namespace CalcLibraryTests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private Calculator _calculator;
+
+        [SetUp]
+        public void init()
+        {
+            _calculator = new Calculator();
+            TestContext.WriteLine("Setup created");
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            TestContext.WriteLine("TearDown: Test Complete");
+        }
+
+        [Test]
+        public void Add_TwoPositiveNums_ReturnSum()
+        {
+            int result = _calculator.add(24, 26);
+            Assert.That(result, Is.EqualTo(50));
+        }
+
+[TestCase(15, 35, 50)]
+[TestCase(-7, -13, -20)]
+[TestCase(100, -50, 50)]
+[TestCase(0, 25, 25)]
+[TestCase(250, 250, 500)]
+        public void Add_MultipleNums_ReturnSum(int x, int y, int expected)
+        {
+            int result = _calculator.add(x, y);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [Ignore("This test is ignored for demonstration")]
+        public void Add_IgnoreThisTest()
+        {
+            Assert.Fail("This test is ignored and should not run.");
+        }
+    }
+}
