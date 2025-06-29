307b44a501e43a8a1f1bd2ee876106d3baf45095
Somya Singh <somya121220@gmail.com>
2025-06-29 19:45:54 +0530

    Add files via upload

---
+++ Week 2/Ranking and Window Functions/Moq/code/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace CustomerCommLib
+{
+    // Interface to be mocked
+    public interface IMailSender
+    {
+        bool SendMail(string toAddress, string message);
+    }
+
+    // Concrete implementation (will NOT be called during test)
+    public class MailSender : IMailSender
+    {
+        public bool SendMail(string toAddress, string message)
+        {
+            MailMessage mail = new MailMessage();
+            SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+
+            mail.From = new MailAddress("your_email@gmail.com");
+            mail.To.Add(toAddress);
+            mail.Subject = "Test Mail";
+            mail.Body = message;
+
+            smtpServer.Port = 587;
+            smtpServer.Credentials = new NetworkCredential("username", "password");
+            smtpServer.EnableSsl = true;
+
+            smtpServer.Send(mail);
+            return true;
+        }
+    }
+
+    // Class under test
+    public class CustomerComm
+    {
+        private IMailSender _mailSender;
+
+        public CustomerComm(IMailSender mailSender)
+        {
+            _mailSender = mailSender;
+        }
+
+        public bool SendMailToCustomer()
+        {
+            return _mailSender.SendMail("cust123@abc.com", "Some Message");
+        }
+    }
+}
