6f226cf97b15e4ec8f3c046eaec7d1029b2d7391
Somya Singh <somya121220@gmail.com>
2025-08-05 11:39:24 +0530

    Add files via upload

---
+++ Week 6/Lab 2/code/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function About() {
+  return (
+    <div>
+      <h2>This portal helps manage students – About Page</h2>
+    </div>
+  );
+}
+export default About;
---
+++ Week 6/Lab 2/code/Contact.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function Contact() {
+  return (
+    <div>
+      <h2>Contact us via this Student Portal page</h2>
+    </div>
+  );
+}
+export default Contact;
